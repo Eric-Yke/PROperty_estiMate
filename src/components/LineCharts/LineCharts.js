5b0ddf155a49635d515d96ab0c93ae9f8898c724
mia <mw771@uowmail.edu.au>
2023-08-21 22:31:35 +1000

    更改了PropertyResult.js的页面的设计布局和components部分的折线图组件

--- src/components/LineCharts/LineCharts.js
+++ src/components/LineCharts/LineCharts.js
@@ -1,22 +1,52 @@
 import React from 'react';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine, Area } from 'recharts';
 
+// const data = [
+//   { year: 2019, price: 438000 },
+//   { year: 2020, price: 450000 },
+//   { year: 2021, price: 390000 },
+//   { year: 2022, price: 366843 },
+//   { year: 2023, price: 420000 },
+//   { year: 2024, price: 440000 },
+//   { year: 2025, price: 465689 }
+// ];
 const data = [
-  { name: 'Jan', uv: 4000, pv: 2400, amt: 2400 },
-  { name: 'Feb', uv: 3000, pv: 1398, amt: 2210 },
-  { name: 'Mar', uv: 2000, pv: 9800, amt: 2290 },
+  { year: 2019, history: 438000, predict: null },
+  { year: 2020, history: 450000, predict: null },
+  { year: 2021, history: 390000, predict: null },
+  { year: 2022, history: 366843, predict: null },
+  { year: 2023, history: 420000, predict: 420000 },
+  { year: 2024, history: null, predict: 450000 },
+  { year: 2025, history: null, predict: 470000 }
 ];
 
-const LineCharts = () => (
-  <LineChart width={500} height={300} data={data}>
-    <CartesianGrid stroke="grey" strokeDasharray="5 5" />
-    <XAxis dataKey="name" />
-    <YAxis />
-    <Tooltip />
-    <Legend />
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" name="Suburb" />
-    <Line type="monotone" dataKey="pv" stroke="#82ca9d" name="The Property" />
-  </LineChart>
-);
+const LineCharts = ({ width, height }) => {
+  return (
+    <LineChart width={width} height={height} data={data}>
+      <CartesianGrid strokeDasharray="3 3" vertical={false} />
+      <XAxis dataKey="year" tickLine={false} tick={{ fontSize: 16 }} tickMargin={15} />
+      <YAxis domain={[360000, 470000]} axisLine={false} tickLine={false} tick={{ fontSize: 14 }} />
+      <Tooltip />
+      <ReferenceLine x="2023" stroke="black" />
+
+      <Area type="monotone" dataKey="history" stroke="#8884d8" fill="url(#colorUv)" data={data.slice(0, 5)} />
+
+      {/* 实线部分 */}
+      <Line type="monotone" dataKey="history" stroke="#8884d8" dot={false} strokeWidth={3} />
+
+      {/* 虚线部分，从第5个点开始 */}
+      <Line type="monotone" dataKey="predict" stroke="#8884d8" dot={false} strokeDasharray="5 5" strokeWidth={3} />
+
+      {/* 渐变底色的定义 */}
+      <defs>
+        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+          <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+
+    </LineChart>
+  );
+};
 
 export default LineCharts;
--- src/pages/PropertyResult.js
+++ src/pages/PropertyResult.js
@@ -1,169 +1,85 @@
-// import React from 'react';
-// import NavBar from '../components/NavBar/NavBar';
-// import Footer from '../components/Footer/Footer';
-// import LineCharts from '../components/LineCharts/LineCharts'; // 引入LineCharts组件
-// import '../pages/PropertyResult.css';
-
-// const PropertyResult = () => {
-//   return (
-//     <>
-//       <NavBar />
-//       <div className="container-main">
-//         <div className="top-section">
-//           <div className="left-box">
-//             <div className="bedroom-info">Bedroom in house</div>
-//             <div className="garage-info">Garage in house</div>
-//             <div className="bathroom-info">Bathroom in house</div>
-//           </div>
-//           <div className="right-box">
-//             <h3 id="property-title">Unit 4 70 Rowland Ave, Wollongong 2500 NSW</h3>
-//             <div className="chart-table-container">
-//               <div className="chart-section">
-//                 <LineCharts />
-//               </div>
-//               <div className="table-section">
-//                 <table className="info-table">
-//                   <tbody>
-//                     <tr>
-//                       <td className="yearly-cell">Yearly</td>
-//                       <td className="monthly-cell">Monthly</td>
-//                     </tr>
-//                     <tr><td colSpan={2}>Rental</td></tr>
-//                     <tr><td colSpan={2}>Suburb Price</td></tr>
-//                     <tr><td colSpan={2}>State Price</td></tr>
-//                     <tr><td className="filter-cell" colSpan={2}>filter</td></tr>
-//                   </tbody>
-//                 </table>
-//               </div>
-//             </div>
-//             <div className="open-map-button">
-//               <button>Open on map</button>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="bottom-section">
-//           <div className="more-info">more inform later..</div>
-//         </div>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default PropertyResult;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//之前使用行内样式的代码
 import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
 import Footer from '../components/Footer/Footer';
-
-import LineCharts from '../components/LineCharts/LineCharts'; // 引入LineCharts组件
-import '../pages/PropertyResult.css'; // 引入CSS文件
+import LineCharts from '../components/LineCharts/LineCharts';
+import './PropertyResult.css';
+import { useRef, useEffect, useState } from 'react';
 
 
 const PropertyResult = () => {
+
+  const cardDivRef = useRef(null);
+  const [chartWidth, setChartWidth] = useState(0);
+  const [chartHeight, setChartHeight] = useState(0);
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      if (cardDivRef.current) {
+        setChartWidth(cardDivRef.current.offsetWidth * 0.9);
+        setChartHeight(cardDivRef.current.offsetHeight * 0.8);
+      }
+    };
+
+    // 首次加载时执行
+    handleResize();
+
+    // 添加窗口大小变化的事件监听器
+    window.addEventListener('resize', handleResize);
+
+    // 在组件卸载时移除事件监听器
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+
   return (
     <>
       <NavBar />
 
-      <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <div style={{ display: "flex", width: "90%", backgroundColor: "#f3f1ea", marginTop: "10px" }}>
-          <div style={{ flex: 2, backgroundColor: "#ffffff", margin: "10px" }}>
+      <div className="card">
+        <div className="filter-buttons">
+          <button className="button">5 year</button>
+          <button className="button">10 year</button>
+          <button className="button">suburb</button>
+          <button className="button">state</button>
+        </div>
 
-            <div style={{
-              backgroundColor: "#ebcab9",
-              color: 'white',
-              radius: "15%",
-              padding: '10px',
-              margin: '5px',
-              textAlign: 'center'
-            }}>
-              Bedroom in house
-            </div>
-            <div style={{
-              backgroundColor: "#e1d3c9",
-              color: 'white',
-              radius: "15%",
-              padding: '10px',
-              margin: '5px',
-              textAlign: 'center'
-            }}>
-              Garage in house
-            </div>
-            <div style={{
-              backgroundColor: "#c1d3d8",
-              color: 'white',
-              radius: "15%",
-              padding: '10px',
-              margin: '5px',
-              textAlign: 'center'
-            }}>
-              Bathroom in house
-            </div>
+        <div className="chart-section" ref={cardDivRef}>
+          <LineCharts width={chartWidth} height={chartHeight} />
+        </div>
+
+        <div className="details-section">
+          <div className="details-left">
+            Address Details Here...
           </div>
-          <div style={{ flex: 8, backgroundColor: "#e1d3c9", margin: "10px" }}>
-            <h3 id="property-title">Unit 4 70 Rowland Ave, Wollongong 2500 NSW</h3>
-
-            <div id="conta002" style={{ display: "flex", flexDirection: "row", gap: '10px' }}>
-              {/* 折线图 */}
-              <div id="lineChart" style={{ flex: 6, padding: '10px', background: 'rgba(255, 255, 255, 0.4)' }}>
-                <LineCharts /> {/* 在这里插入LineCharts组件 */}
-              </div>
-
-              {/* 表格 */}
-              {/* <div id="tableSimple" style={{ flex: 4, padding: '10px' }}> ..</div> */}
-              <div id="tableSimple" style={{ flex: 4, padding: '10px', display: 'flex', justifyContent: 'flex-start' }}>
-                <table style={{ borderCollapse: 'collapse', width: '50%', padding: '15px' }}>
-                  <tbody>
-                    <tr>
-                      <td style={{ color: '#8e8e8efb', backgroundColor: '#eeeeeefd', border: '1px solid #ffffff', textAlign: 'center', verticalAlign: 'middle' }}>Yearly</td>
-                      <td style={{ color: '#8e8e8efb', backgroundColor: '#f4ecd8fe', border: '1px solid #ffffff', textAlign: 'center', verticalAlign: 'middle' }}>Monthly</td>
-                    </tr>
-                    <tr>
-                      <td style={{ color: '#8e8e8efb', backgroundColor: '#eeeeeefd', border: '1px solid #ffffff', textAlign: 'center', verticalAlign: 'middle' }} colSpan={2}>Rental</td>
-                    </tr>
-                    <tr>
-                      <td style={{ color: '#8e8e8efb', backgroundColor: '#f4ecd8fe', border: '1px solid #ffffff', textAlign: 'center', verticalAlign: 'middle' }} colSpan={2}>Suburb Price</td>
-                    </tr>
-                    <tr>
-                      <td style={{ color: '#8e8e8efb', backgroundColor: '#eeeeeefd', border: '1px solid #ffffff', textAlign: 'center', verticalAlign: 'middle' }} colSpan={2}>State Price</td>
-                    </tr>
-                    <tr>
-                      <td style={{ color: '#ffffff', backgroundColor: '#f1713a', border: '1px', textAlign: 'center', verticalAlign: 'middle', fontWeight: 'bold' }} colSpan={2}>filter</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-
-
-            <div style={{ display: "flex", justifyContent: "flex-end", padding: "20px" }}>
-              <button>Open on map</button>
-            </div>
+          <div className="details-right">
+            <br ></br>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>LOW</th>
+                  <th style={{ fontSize: '1.1em' }}>MID</th>
+                  <th>HIGH</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>$980K</td>
+                  <td className="special-price">$1.14M</td>
+                  <td>$1.3M</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        <div style={{ width: "90%", backgroundColor: "#fbf8ed", marginTop: "10px", height: "400px", }}>
-          <div style={{ backgroundColor: "#ffffff", margin: "10px", height: "96%" }}>
-            more inform later..
-          </div>
-        </div>
-        <Footer />
+      </div>
+      <div className="card-two">
 
       </div>
+
+      <Footer />
     </>
   );
 };
