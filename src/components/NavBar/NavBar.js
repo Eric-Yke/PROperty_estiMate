564151630db812593c94936700f5dfcb609f7197
mia <mw771@uowmail.edu.au>
2023-08-21 23:32:14 +1000

    图标

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,64 +4,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';  // 引入 Link
 import './NavBar.css';
 import logoImage from '../../../src/images/logowithouttext.PNG';  // 导入 logo 图片
-
+import { AiOutlineUser, AiOutlineStar, AiOutlineBell, AiOutlineSwap } from "react-icons/ai";
 
 const NavBar = () => (
   <nav className="navbar">
     <div className="navbar-section">
-      {/* <div className="logo">LOGO</div> */}
+
       <img src={logoImage} alt="LOGO" className="logo-img" />
-      <div className="location">Wollongong</div>
+
+      <div className="location" style={{ fontSize: 20 }}>Wollongong</div>
+      <AiOutlineSwap className="icon" size={22} color={'#f1713a'} />
     </div>
+
     <div className="navbar-section">
-      <Link to="/search">search</Link>
-      <Link to="/news">news</Link>
-      <Link to="/community">community</Link>
-      <Link to="/setting">setting</Link>
-    </div>
-    <div className="navbar-section">
-      <Link to="/notification">notification</Link>
-      <Link to="/collections">collections</Link>
-      <Link to="/login">my account</Link>
+      <Link to="/notification">
+        <AiOutlineBell className="icon" size={28} />
+        <div className="icon-text">Notification</div>
+      </Link>
+      <Link to="/collections">
+        <AiOutlineStar className="icon" size={28} />
+        <div className="icon-text">Collections</div>
+      </Link>
+      <Link to="/login">
+        <AiOutlineUser className="icon" size={28} />
+        <div className="icon-text">My Account</div>
+      </Link>
     </div>
   </nav>
 );
 
 export default NavBar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import './NavBar.css';
-
-// const NavBar = () => (
-//   <nav className="navbar">
-//     <div className="navbar-section">
-//       <div className="logo">LOGO</div>
-//       <div className="location">Wollongong</div>
-//     </div>
-//     <div className="navbar-section">
-//       <a href="#">search</a>
-//       <a href="#">news</a>
-//       <a href="#">community</a>
-//       <a href="#">setting</a>
-//     </div>
-//     <div className="navbar-section">
-//       <a href="#">notification</a>
-//       <a href="#">collections</a>
-//       <a href="#">my account</a>
-//     </div>
-//   </nav>
-// );
-
-// export default NavBar;
